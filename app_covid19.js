595afef407dbd2e755734cd278ad55ec45b10db6
るんるんけだま <runrunkedama@runrunkedamanoMacBook.local>
2020-05-07 12:26:29 +0900

    update

--- app_covid19.js
+++ app_covid19.js
@@ -3,6 +3,8 @@
 
 const axios = require('axios');
 const fs = require('fs'); //注: npm i 不要
+//const fs_r = require('fs_r'); //注: npm i 不要
+
 
 //データ更新関数
 async function updateData(newData){
@@ -16,6 +18,7 @@ async function getRequest() {
     const response = await axios.get(`https://api.covid19api.com/summary`);
     var TC = response.data.Global.TotalConfirmed
     console.log('TotalConfirmed:' + TC);
+   // TC.writeFileSync(PATH, JSON.stringify(newData);
    // console.log(response.data.Countries);
   } catch (error) {
     console.error(error);
